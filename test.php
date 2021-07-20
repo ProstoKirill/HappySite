29a1574ecd241d3907c88355270d9db916cfaf35
ProstoKirill <87670090+ProstoKirill@users.noreply.github.com>
2021-07-20 13:35:29 +0300

    Add files via upload

---
+++ test.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="Welcome to HappyHTML!">
+	  <meta name="keywords" content="WorldHTML">
+  	<meta name="author" content="Brad Traversy">
+    <title>HappyHTML | Welcome</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="icon" href="img/icoHTML.jpg">
+  </head>
+  <body>
+<php>    <header>
+      <div class="container">
+        <div id="branding">
+          <h1><span class="highlight">Happy</span>HTML</h1>
+        </div>
+        <nav>
+          <ul>
+            <li class="current"><a href="home.html">Home</a></li>
+            <li><a href="information.html">information</a></li>
+            <li><a href="donate.html">Donate</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+<section id="showcase">
+      <div class="container">
+        <h1>This is HTML</h1>
+        <p>Hello in my HTML!</p>
+      </div>
+    </section>
+     <footer>
+      <p>HappyHTML&copy; 2021</p>
+    </footer>
+    </php>
+</body>
